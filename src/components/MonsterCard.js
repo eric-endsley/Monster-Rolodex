bb83f76644d2a328136a552a0c0feb09b20ec1da
Eric Endsley <eric.endsley4@gmail.com>
2021-11-18 21:29:02 -0800

    continue building out statblock display

--- src/components/MonsterCard.js
+++ src/components/MonsterCard.js
@@ -11,11 +11,9 @@ export default function MonsterCard(props) {
         <Text style={cardStyles.name}>{monster.name}</Text>
         <Text style={cardStyles.subtitle}>{monster.size} {monster.type}, {monster.alignment}</Text>
         <View style={cardStyles.redLine} />
-        <Text style={cardStyles.redStats}>
-          <Text><Text style={cardStyles.bold}>Armor Class</Text> <Text>{monster.ac}</Text></Text>
-          <Text><Text style={cardStyles.bold}>Hit Points</Text> <Text>{monster.hp}</Text></Text>
-          <Text><Text style={cardStyles.bold}>Speed</Text> <Text>{monster.speed}</Text></Text>
-        </Text>
+        <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Armor Class</Text> <Text>{monster.ac}</Text></Text></View>
+        <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Hit Points</Text> <Text>{monster.hp}</Text></Text></View>
+        <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Speed</Text> <Text>{monster.speed}</Text></Text></View>
         <View style={cardStyles.redLine} />
         <View style={cardStyles.abilities}>
           <View style={cardStyles.abilityBlock}>
@@ -42,10 +40,21 @@ export default function MonsterCard(props) {
             <Text style={cardStyles.ability}>CHA</Text>
             <Text style={cardStyles.abilityScore}>{monster.cha} ({Math.floor(((monster.cha - 10)/2))})</Text>
           </View>
-          <View style={cardStyles.redLine} />
         </View>
-      </View>
+        <View style={cardStyles.redLine} />
+        {(monster.save[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Saving Throws</Text> <Text>{monster.save}</Text></Text></View>}
+        {(monster.skill[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Skills</Text> <Text>{monster.skill}</Text></Text></View>}
+        {(monster.vulnerable[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Damage Vulnerabilities</Text> <Text>{monster.vulnerable}</Text></Text></View>}
+        {(monster.resist[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Damage Resistances</Text> <Text>{monster.resist}</Text></Text></View>}
+        {(monster.immune[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Damage Immunities</Text> <Text>{monster.immune}</Text></Text></View>}
+        {(monster.conditionImmune[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Condition Immunities</Text> <Text>{monster.conditionImmune}</Text></Text></View>}
+        <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Senses</Text> <Text>{monster.senses[0] !== "" && monster.senses} Passive Perception {monster.passive}</Text></Text></View>
+        {(monster.languages[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Languages</Text> <Text>{monster.languages}</Text></Text></View>}
+        {(monster.cr[0] !== "") && <View><Text style={cardStyles.redStats}><Text style={cardStyles.bold}>Challenge</Text> <Text>{monster.cr}</Text></Text></View>}
+        <View style={cardStyles.redLine} />
+        {/* {monster.trait != null && monster.trait.map(trait => <View style={{flex:1}}><Text style={cardStyles.boldItalic}>{trait.name}</Text> <Text style={cardStyles.blackStats}>{trait.text}</Text></View>)} */}
     </View>
+  </View> 
   )
 }
 
@@ -82,6 +91,15 @@ const cardStyles = StyleSheet.create({
   redStats: {
     color: '#922610',
     fontFamily: 'Bookmania',
+    marginTop: -5,
+    marginBottom: -5,
+  },
+  blackStats: {
+    color: 'black',
+    fontFamily: 'Bookmania',
+  },
+  boldItalic: {
+    fontFamily: 'BookmaniaBoldItalic'
   },
   bold: {
     fontFamily: 'BookmaniaBold'
